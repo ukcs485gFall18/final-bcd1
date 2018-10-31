7820a3ab03f0b7bbd26d2f55a97ab53f97733555
Blake <blake.sweet16@gmail.com>
2018-10-30 21:10:45 -0400

    Added reservation variable types

--- ForgetMeNot/ViewControllers/SignUpViewController.swift
+++ ForgetMeNot/ViewControllers/SignUpViewController.swift
@@ -15,16 +15,19 @@ class SignUpViewController : UIViewController{
     @IBOutlet weak var userPass: UITextField!
     @IBOutlet weak var userPassConfirm: UITextField!
     
+    func showError (alertTitle : String, alertMessage : String, actionTitle : String){
+        let alertController = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: actionTitle, style: .cancel, handler: nil)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     @IBAction func signUpAction(_ sender: Any) {
         if (userPass.text == userPassConfirm.text){ // Verify both passwords match
         Auth.auth().createUser(withEmail: userEmail.text!, password: userPass.text!)
         }
         else{
-            let alertController = UIAlertController(title: "Passwords do not match", message: "Please make sure both passwords are identical.", preferredStyle: .alert)
-            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            
-            alertController.addAction(defaultAction)
-            self.present(alertController, animated: true, completion: nil)
+            showError(alertTitle: "Passwords do not match", alertMessage: "Please enter identical passwords", actionTitle: "Dismiss")
         }
     }
 }
