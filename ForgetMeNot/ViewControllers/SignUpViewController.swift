96c5b546a17b8125cee740a95ea2fd5cc365c36c
Blake <blake.sweet16@gmail.com>
2018-10-30 20:28:00 -0400

    Verify that passwords match when signing up

--- ForgetMeNot/ViewControllers/SignUpViewController.swift
+++ ForgetMeNot/ViewControllers/SignUpViewController.swift
@@ -16,6 +16,15 @@ class SignUpViewController : UIViewController{
     @IBOutlet weak var userPassConfirm: UITextField!
     
     @IBAction func signUpAction(_ sender: Any) {
+        if (userPass.text == userPassConfirm.text){ // Verify both passwords match
         Auth.auth().createUser(withEmail: userEmail.text!, password: userPass.text!)
+        }
+        else{
+            let alertController = UIAlertController(title: "Passwords do not match", message: "Please make sure both passwords are identical.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
 }
