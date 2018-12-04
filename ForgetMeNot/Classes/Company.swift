e29f4af2a221c292d922b39972daf52143b2fc88
David Mercado <dme236@g.uky.edu>
2018-12-04 17:48:57 -0500

    adding firebase capabilities to company class

--- ForgetMeNot/Classes/Company.swift
+++ ForgetMeNot/Classes/Company.swift
@@ -5,6 +5,7 @@
 //
 
 import Foundation
+import Firebase
 
 // Created Company model for retrieving data (Host)
 class Company/*: NSObject*/{
@@ -82,5 +83,57 @@ class Company/*: NSObject*/{
     func sortReservation(){
         //sort reservations by date time
     }
-}
+    
+    /*====================================================
+    *               FireBase Functions
+    *       populate the data for the company
+    ====================================================*/
+    
+    func populateSelf() {
+        let dataRef = Database.database().reference()
 
+        
+        dataRef.child("companyList").child(companyName).observe(.value) { (datasnapshot) in
+            guard let partySnapshot = datasnapshot.children.allObjects as? [DataSnapshot] else { return }
+            print("SnapShot: \(partySnapshot)")                                     //DEBUGGING PURPOSES
+            
+            //iterate through the Company's reservation table
+            for currParty in partySnapshot{
+                print("currParty: \(currParty.key)")
+                guard let reservations = currParty.value as? [String:Any] else{
+                    return
+                }
+                print("reservations: \(reservations)")                              //DEBUGGING PURPOSES
+                // Each reservation referenced inside loop
+                for reservation in reservations{
+                    // Store variables as a dictionary
+                    print("reservation: \(reservation)")                            //DEBUGGING PURPOSES
+                    #warning("FIX ME!!!")
+                    /* let partyValues = reservation.value as! [String:Any] //else{
+                     //return
+                     //}   // Dictionary containing each value pair of res
+                     
+                     // Collect variables from database
+                     let currResUUID_Str = reservation.key
+                     let currResDate = partyValues["partyDate"] as! String
+                     let currResName = partyValues["partyName"] as! String
+                     let currResSize = partyValues["partySize"] as! Int
+                     
+                     // Convert UUIDString back to a normal UUID to be placed into reservation
+                     let currResUUID = UUID(uuidString: currResUUID_Str)!
+                     
+                     print("UUID_Str: \(currResUUID_Str)")
+                     print("currResDate: \(currResDate)")
+                     print("currResName: \(currResName)")
+                     print("currResSize: \(currResSize)")
+                     
+                     //Convert data into a MyReservation NSObject
+                     let currRes = MyReservation(date: currResDate, uuid: currResUUID, CompName: Host.getName(), name: currResName, size: currResSize)
+                     
+                     //Add the reservation to the Company's list
+                     Host.appendReservation(customerRes: currRes)*/
+                }
+            }
+        }
+    }
+}
