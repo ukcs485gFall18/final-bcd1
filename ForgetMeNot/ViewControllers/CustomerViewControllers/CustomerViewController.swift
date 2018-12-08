f25bc30b7d1cc5f73480091b3c3fcd63095a1a8f
Blake <blake.sweet16@gmail.com>
2018-12-08 02:43:25 -0500

    Push for Baker

--- ForgetMeNot/ViewControllers/CustomerViewControllers/CustomerViewController.swift
+++ ForgetMeNot/ViewControllers/CustomerViewControllers/CustomerViewController.swift
@@ -14,14 +14,30 @@ class CustomerViewController : UIViewController, UITableViewDelegate, UITableVie
     
     // Local Variables
     var myCustomer : Users = Users(email: "", userType: "")
+    let dataRef = Database.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Collect user data for table
         myCustomer.userID = Auth.auth().currentUser!.uid
-        myCustomer.partyNames = myCustomer.loadPartyNames() // Load all parties to the user
-        myCustomer.reservationList = myCustomer.loadReservations() // Load all reservations on the user
+        
+        // Load all parties to the user
+        //myCustomer.partyNames = myCustomer.loadPartyNames()
+        // Create firebase reference and link to database
+        
+        dataRef.child("userList/\(myCustomer.userID!)/partyNameList").observe(.value) { (datasnapshot) in
+            guard let partynamesnapshot = datasnapshot.children.allObjects as? [DataSnapshot] else { return }
+            
+            for eachPartyName in partynamesnapshot {
+                guard let newpartyName : String = eachPartyName.childSnapshot(forPath: "partyName").value as? String else{return}
+                self.myCustomer.partyNames.append(newpartyName)
+                print("DEBUGGING: Added \(newpartyName) to (handler) partyNames")
+            }
+        }
+        
+        // Load all reservations on the user
+        myCustomer.reservationList = myCustomer.loadReservations()
         
         customerReservationTableView.dataSource = self
         customerReservationTableView.delegate = self
